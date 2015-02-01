bf6e7da2dc1f40287b92ad9ff37a9b0aef20b93f
CeltonMcGrath <mcgrathcelton@gmail.com>
2015-01-31 22:25:41 -0500

    IT WORKS

--- index.js
+++ index.js
@@ -5,46 +5,51 @@ var path = require('path');
 
 
 // Set up Twitter
-var Twitter = require('twitter');
-
-
+var Twit = require('twit');
 
 twitterCreds = {
   consumer_key: '9tJIrpsuuNGRYlGMpdBmfviau',
   consumer_secret: '4yWuUdJmhWTU0tPKcTI9DgbLSb2WMtfOKkzuXff2YI62UNLBgh',
-  access_token_key: '1646397260-TXHpIMv1IKDAJ4auiCHHCbDsJaYa33Q8mKIHN14',
+  access_token: '1646397260-TXHpIMv1IKDAJ4auiCHHCbDsJaYa33Q8mKIHN14',
   access_token_secret: 'mL2XkXjqiWgvqFuhWeNmcX7Q3uqijKPhzI4LhaCQiF4U7'
 };
 
-var client = new Twitter(twitterCreds);
+var T = new Twit(twitterCreds);
+var stream = T.stream('statuses/sample');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 var text = "Superbowl";
-//********
+//var ne = ;
+//var sw = ;
+//  ********
 
 io.on('connection', function(socket) {
   socket.on('new subject', function(text) {
-    client.stream('statuses/filter', {track: text}, function(stream) {
-      stream.on('data', function(tweet) {
-        console.log(text + ': ' + tweet.coordinates);
-        io.sockets.emit('new tweet', tweet.coordinates);
-      });
+    stream.stop();
+    stream = T.stream('statuses/filter', {track: text})
+    stream.on('tweet', function (tweet) {
+      console.log(text + " : " + tweet.text);
+      io.sockets.emit('new tweet', tweet.coordinates);
+    }); 
 
-      stream.on('error', function(error) {
-       console.log("error");
-      });
+    stream.on('error', function(error) {
+     console.log(error);
+    });
       
-     });
    });
 
   socket.on('close stream', function(text) {
     console.log("close");
-    stream.destroy();
-    stream = null;
+    //T.stream.stop()
   });
+
+  socket.on('new boundary', function(newNe, newSw) {
+    
+  });
+
 });
 
 
